edb3abf68f5201ac9c223dab0d708f0c7c8c7616
mhyang721 <112519429+mhyang721@users.noreply.github.com>
2023-07-28 22:46:57 -0700

    Add custom typography styles

--- public/partials/global/head.php
+++ public/partials/global/head.php
@@ -4,8 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/reset.css">
     <title>Hisani | Omakase Restaurant in Vancouver</title>
-    <link rel="stylesheet" href="style/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400&family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <?php require(get_path('public/partials/global/tailwind.php')); ?>
+    <link rel="stylesheet" href="../src/input.css">
 </head>
--- public/partials/global/tailwind.php
+++ public/partials/global/tailwind.php
@@ -3,11 +3,41 @@
         theme: {
             extend: {
                 colors: {
-                red: '#D63241',
-                blue: '#2D4263',
-                black: '#191919',
-                white: '#FBFBFB',
-                }
+                red: 'var(--red)',
+                black: 'var(--black)',
+                white: 'var(--white)',
+                },
+                fontFamily: {
+                    'lora': ['Lora', 'serif'],
+                    'nunito': ['Nunito', 'sans-serif'],
+                },
+                fontSize: {
+                    // mobile
+                    h1: '28px',
+                    h2: '34px',
+                    h3: '24px',
+                    h4: '24px',
+                    p: '17px',
+                    // (mobile hamburger nav menu)
+                    hamtext: '34px',
+                    // body-medium (mobile food menu)
+                    menutext: '22px',
+                    // body-small (mobile footer)
+                    footertext: '16px',
+
+                    // desktop
+                    mdh1: '34px',
+                    mdh2: '60px',
+                    mdh3: '30px',
+                    mdh4: '28px',
+                    mdp: '24px',
+                    // body-small (desktop nav menu) 
+                    mdhamtext: '22px',
+                    // body-small (desktop food menu) 
+                    mdmenutext: '22px',
+                    // body-extra-small (desktop footer)
+                    mdfootertext: '16px',
+                },
             }
         }
     }
