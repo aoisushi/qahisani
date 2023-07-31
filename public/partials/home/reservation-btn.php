4bf730df423341cb75ae9c19d990717abcaeced8
mhyang721 <112519429+mhyang721@users.noreply.github.com>
2023-07-31 16:52:27 -0700

    Create homepage reservation btn

---
+++ public/partials/home/reservation-btn.php
@@ -0,0 +1,3 @@
+<div class="bg-black pb-20 md:pb-32 text-center px-5">
+    <a href="reservations.php" class="p font-bold bg-black outline outline-1 outline-white text-white uppercase py-6 max-w-md mx-auto flex justify-center items-center hover:bg-red hover:outline-none transition transition-all duration-500" title="Make a reservation">Reserve a Table</a>
+</div>
