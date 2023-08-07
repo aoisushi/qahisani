4418caa4e9c9d32455a222a407de4170cd7a9572
mhyang721 <112519429+mhyang721@users.noreply.github.com>
2023-08-06 21:58:08 -0700

    Fix header nav not working on reservation pgs
    - Updated href attribute

--- public/partials/global/header.php
+++ public/partials/global/header.php
@@ -8,10 +8,10 @@
             <div class="nav-links duration-500 absolute md:static bg-black min-h-[60vh] md:min-h-fit left-0 top-[-100%] w-full md:w-auto flex items-center px-5">
                 <ul class="flex md:flex-row flex-col gap-6 md:items-center md:gap-[4vw]">
                     <!-- <li class="nav-item"><a href="#" class="nav-link">Home</a></li> -->
-                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="#about">About</a></li>
-                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="#menu">Menu</a></li>
-                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="#gallery">Gallery</a></li>
-                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="#contact">Contact</a></li>
+                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="index.php#about">About</a></li>
+                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="index.php#menu">Menu</a></li>
+                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="index.php#gallery">Gallery</a></li>
+                    <li><a class="text-mdhamtext font-nunito p-2 hover:text-[#FFE4E0] hover-stroke" href="index.php#contact">Contact</a></li>
                     <li><a href="reservations.php"><button class="font-nunito bg-[#D63241] text-mdhamtext text-white px-[80px] py-[15px] mx-2 md:px-[20px] md:py-[10px] hover:bg-[#FFE4E0] hover:text-black ease-in-out duration-300">Reservation</button></a></li>
                 </ul>
             </div>
